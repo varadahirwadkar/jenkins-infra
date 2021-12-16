79297a7b48928214d643fa6109227c4e4d422e0f
ShilpiDas <shilpi12@in.ibm.com>
2021-12-16 17:09:51 +0530

    Fix get client and build methods

--- vars/getOpenshiftClientAMD.groovy
+++ vars/getOpenshiftClientAMD.groovy
@@ -7,14 +7,12 @@
      def min4 = ~/\[/
      def ls =''
      build.eachLine{
-         if (it =~ /^<tr/) {
-             String lst = it.findAll(/<a href=".*/)
-             if (lst.contains("openshift-client-linux.tar.gz")) {
-                         ls = ls + " " + (lst - min1)
-                         ls = ls - min3
-                         ls = ls - min4
-             }
-         }
+        String lst = it.findAll(/<a href=".*/)
+        if (lst.contains("openshift-client-linux.tar.gz")) {
+            ls = ls + " " + (lst - min1)
+            ls = ls - min3
+            ls = ls - min4
+        }
      }
      //bld = ls.split() as List
      def lsnew = ls.trim()
